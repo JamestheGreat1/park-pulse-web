0ad31f27552338c5780b88441d1fe28aec765eee
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-22 17:32:05 -0600

    Refresh push module for ParkPulse 1.3.1

--- assets/js/push.js
+++ assets/js/push.js
@@ -1,4 +1,4 @@
-import { workerBase } from "./api.js?v=1.3.0";
+import { workerBase } from "./api.js?v=1.3.1";
 
 function base64ToBytes(value) {
   const padded = value.padEnd(value.length + (4 - value.length % 4) % 4, "=").replace(/-/g, "+").replace(/_/g, "/");

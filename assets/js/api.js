2459560a54d4b7aacce98baccc555e4f41a45762
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-22 16:48:09 -0600

    Finalize ThemeParks migration cache version

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.2.0";
+import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.2.1";
 
 const config = window.PARKPULSE_CONFIG || {};
 export const workerBase = String(config.WORKER_BASE || "").replace(/\/$/, "");

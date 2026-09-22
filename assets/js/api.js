490cb5c910b1a3615a95006fd921862c6838c2b6
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-22 17:51:12 -0600

    Align API module cache with 1.3.4

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.3.3";
+import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.3.4";
 
 const config = window.PARKPULSE_CONFIG || {};
 export const workerBase = String(config.WORKER_BASE || "").replace(/\/$/, "");

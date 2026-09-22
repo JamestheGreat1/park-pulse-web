c8bd9666037719c35e84ec867c3a205acf82b127
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-22 17:57:56 -0600

    Refresh assets/js/api.js for ParkPulse 1.3.5

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.3.4";
+import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.3.5";
 
 const config = window.PARKPULSE_CONFIG || {};
 export const workerBase = String(config.WORKER_BASE || "").replace(/\/$/, "");

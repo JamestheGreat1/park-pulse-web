96af2e75c44c042bfb171e22227c8c7b5a5f4733
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-23 17:38:49 -0600

    Bump ParkPulse runtime cache

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.6.0-product-polish";
+import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.6.1-runtime-fix";
 
 const config = window.PARKPULSE_CONFIG || {};
 export const workerBase = String(config.WORKER_BASE || "").replace(/\/$/, "");

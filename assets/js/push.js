07328ff600dd968f800c958d0dc5c6a5d00d7401
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-24 18:37:58 -0600

    Bump ParkPulse push module cache

--- assets/js/push.js
+++ assets/js/push.js
@@ -1,4 +1,4 @@
-import { workerBase } from "./api.js?v=1.7.13";
+import { workerBase } from "./api.js?v=1.7.14";
 
 function base64ToBytes(value) {
   const padded = value.padEnd(value.length + (4 - value.length % 4) % 4, "=").replace(/-/g, "+").replace(/_/g, "/");

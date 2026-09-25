9211845573d179a69464531e58b1137b4f90e957
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-25 07:34:21 -0600

    Bump ParkPulse to 1.8.7

--- assets/js/push.js
+++ assets/js/push.js
@@ -1,4 +1,4 @@
-import { workerBase } from "./api.js?v=1.8.6";
+import { workerBase } from "./api.js?v=1.8.7";
 
 function base64ToBytes(value) {
   const padded = value.padEnd(value.length + (4 - value.length % 4) % 4, "=").replace(/-/g, "+").replace(/_/g, "/");

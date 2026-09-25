83259444f7ec276e41f43b8f721b8aa29ad1b076
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-24 22:26:39 -0600

    Smooth Minecraft firework rocket trails

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.8.0-preview.17";
+import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.8.0-preview.18";
 
 const config = window.PARKPULSE_CONFIG || {};
 export const workerBase = String(config.WORKER_BASE || "").replace(/\/$/, "");

3723adb4866f718a11de8028593fa72c7f931fe7
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-24 18:37:56 -0600

    Bump ParkPulse API module cache

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.7.13";
+import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.7.14";
 
 const config = window.PARKPULSE_CONFIG || {};
 export const workerBase = String(config.WORKER_BASE || "").replace(/\/$/, "");

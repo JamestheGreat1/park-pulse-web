08aeecc0c0574dfd26106548be97a03bc2c8ea27
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-25 07:47:49 -0600

    Bump ParkPulse to 1.8.9

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.8.8";
+import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.8.9";
 
 const config = window.PARKPULSE_CONFIG || {};
 export const workerBase = String(config.WORKER_BASE || "").replace(/\/$/, "");

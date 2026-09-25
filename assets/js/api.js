d178a500690fcb2ac30431db124efcddeed1ab29
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-25 03:30:21 -0600

    Bump preview assets to 1.9.0-preview.5

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.9.0-preview.4";
+import { PARKS, isSingleRiderName, normalizeRideName } from "./data.js?v=1.9.0-preview.5";
 
 const config = window.PARKPULSE_CONFIG || {};
 export const workerBase = String(config.WORKER_BASE || "").replace(/\/$/, "");

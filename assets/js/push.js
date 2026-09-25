b81733200414193ba4f9725fe4928fa64ee9fff6
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-25 03:48:09 -0600

    Bump preview assets to 1.9.0-preview.9

--- assets/js/push.js
+++ assets/js/push.js
@@ -1,4 +1,4 @@
-import { workerBase } from "./api.js?v=1.9.0-preview.8";
+import { workerBase } from "./api.js?v=1.9.0-preview.9";
 
 function base64ToBytes(value) {
   const padded = value.padEnd(value.length + (4 - value.length % 4) % 4, "=").replace(/-/g, "+").replace(/_/g, "/");

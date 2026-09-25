3ebfcfda34b79cf659b510e407b61676694ed807
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-24 21:04:46 -0600

    Restore preview API endpoint

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.PARKPULSE_CONFIG = Object.freeze({
-  WORKER_BASE: window.location.origin,
+  WORKER_BASE: "https://parkpulse-api.jamesp5297.workers.dev",
   REFRESH_INTERVAL_MS: 5 * 60 * 1000,
   REQUEST_TIMEOUT_MS: 12 * 1000,
   DEFAULT_PARK_ID: 6

c0f2bc8bce920f850889b8c3a8248fc30d531584
James_theGreat1 <68579444+JamestheGreat1@users.noreply.github.com>
2026-09-24 22:55:29 -0600

    Break July 4 trail into independent sparks

--- assets/js/push.js
+++ assets/js/push.js
@@ -1,4 +1,4 @@
-import { workerBase } from "./api.js?v=1.8.0-preview.22";
+import { workerBase } from "./api.js?v=1.8.0-preview.23";
 
 function base64ToBytes(value) {
   const padded = value.padEnd(value.length + (4 - value.length % 4) % 4, "=").replace(/-/g, "+").replace(/_/g, "/");
